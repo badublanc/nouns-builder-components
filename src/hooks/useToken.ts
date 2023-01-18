864f3663a4eb4f5b8e1f60ae059eeeb06dda9565
Badu Blanc <badu@hey.com>
2023-01-18 12:34:57 -0600

    refactor useToken hook

--- src/hooks/useToken.ts
+++ src/hooks/useToken.ts
@@ -1,47 +1,69 @@
 import type { Network } from '../types';
 import { BigNumber } from 'ethers';
 import { useEffect, useState } from 'react';
-import { useContract, useProvider } from 'wagmi';
+import { useContractRead } from 'wagmi';
 import { TokenABI } from '../abis';
+import { getChainIdFromNetwork } from '../utils';
 
 interface UseTokenConfig {
-	id: number;
+	tokenId: number;
 	tokenAddress: string;
 	network?: Network;
 }
 
 export const useToken = ({
-	id,
+	tokenId,
 	tokenAddress,
 	network = 'mainnet',
 }: UseTokenConfig) => {
-	const provider = useProvider();
-	const [imageUrl, setImageUrl] = useState('');
+	const [uri, setUri] = useState<Record<string, any>>({});
+	const [name, setName] = useState<string>('');
+	const [description, setDescription] = useState<string>('');
+	const [imageUrl, setImageUrl] = useState<string>('');
+	const [traits, setTraits] = useState<Record<string, any>>({});
 
-	const contract = useContract({
-		address: tokenAddress as `0x${string}`,
+	const resetValues = () => {
+		setName('');
+		setDescription('');
+		setImageUrl('');
+		setTraits({});
+	};
+
+	useContractRead({
+		address: tokenAddress as `0x$string}`,
+		chainId: getChainIdFromNetwork(network),
 		abi: TokenABI,
-		signerOrProvider: provider,
+		functionName: 'tokenURI',
+		args: [BigNumber.from(tokenId || '0')],
+		enabled: Boolean(tokenAddress && Number.isInteger(tokenId)),
+		onSuccess(data) {
+			const uri = window.atob(data.split(',')[1]);
+			if (uri) setUri(JSON.parse(uri));
+			else setUri({});
+		},
+		onError(err) {
+			console.error(err);
+			setUri({});
+		},
 	});
 
 	useEffect(() => {
-		const getUriData = async (tokenId: number) => {
-			const id = BigNumber.from(tokenId);
-			const data = await contract?.tokenURI(id);
-			if (data) {
-				const uri = window.atob(data.split(',')[1]);
-				const { image } = JSON.parse(uri);
-				if (image) setImageUrl(image);
-				else setImageUrl('');
-			} else setImageUrl('');
-		};
+		const { name, description, image, properties } = uri;
 
-		if (id) {
-			getUriData(id);
-		}
+		if (name) setName(name);
+		else setName('');
 
-		return () => setImageUrl('');
-	}, [id, contract]);
+		if (description) setDescription(description);
+		else setDescription('');
 
-	return { id, imageUrl };
+		if (image) setImageUrl(image);
+		else setImageUrl('');
+
+		if (properties) setTraits(properties);
+		else setTraits({});
+
+		return () => resetValues();
+	}, [uri]);
+
+	return { tokenId, name, description, imageUrl, traits };
 };
