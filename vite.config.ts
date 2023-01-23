d2d2aa071214831537dc3275efa724eb6d670976
Badu Blanc <badu@hey.com>
2023-01-23 17:22:03 -0600

    update build step, remove vite.svg

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: '[name].js',
+				assetFileNames: '[name][extname]',
+				chunkFileNames: 'chunks-[name].js',
+			},
+		},
+	},
+});
