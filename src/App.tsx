8bb229f0240f74f8226f57dc393b5724a0b8ae8d
Badu Blanc <badu@hey.com>
2023-01-23 16:12:33 -0600

    logic updates, add connect button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
-import { BuilderDAO, AuctionHero } from '../lib';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { AuctionHero } from '../lib';
 
-const App = () => {
-	return (
-		<BuilderDAO collection="0x73e769fcd28e66c43f785f5479b4ba8ef9886863" chain="GOERLI">
-			<Routes>
-				<Route path="/auctionHero" element={<AuctionHero />} />
-			</Routes>
-		</BuilderDAO>
-	);
+const App = ({ component }: { component: string }) => {
+	switch (component) {
+		case 'connectButton':
+			return <ConnectButton />;
+		case 'auctionHero':
+			return <AuctionHero />;
+		default:
+			return <></>;
+	}
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,22 @@ import './index.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { WagmiConfig, createClient, configureChains, mainnet, goerli } from 'wagmi';
 import { infuraProvider } from 'wagmi/providers/infura';
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { BuilderDAO } from '../lib';
+
+const config = document.querySelector('[data-builder-config]') as HTMLElement;
+const apiKey = config?.dataset.infuraKey || '';
+
+if (!apiKey) {
+	throw new Error('Infura key required');
+}
 
 const { chains, provider, webSocketProvider } = configureChains(
 	[mainnet, goerli],
-	[infuraProvider({ apiKey: 'cfe94335ab724354835fefb844aa0db7' })]
+	[infuraProvider({ apiKey })]
 );
 
 const { connectors } = getDefaultWallets({
@@ -25,14 +32,31 @@ const client = createClient({
 	webSocketProvider,
 });
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<WagmiConfig client={client}>
-			<RainbowKitProvider chains={chains}>
-				<BrowserRouter>
-					<App />
-				</BrowserRouter>
-			</RainbowKitProvider>
-		</WagmiConfig>
-	</React.StrictMode>
-);
+const roots = document.querySelectorAll('[data-builder-component]');
+const daoNotRequired = ['connectButton'];
+
+for (const root of roots) {
+	const chain = (root as HTMLElement)?.dataset.chain as 'MAINNET' | 'GOERLI';
+	const collection = (root as HTMLElement)?.dataset.daoCollection || '';
+	const component = (root as HTMLElement)?.dataset.builderComponent || '';
+
+	if (!component) {
+		console.warn('component type required.');
+		break;
+	} else if (!daoNotRequired.includes(component) && !collection) {
+		console.warn(`collection required for ${component}`);
+		break;
+	}
+
+	ReactDOM.createRoot(root).render(
+		<React.StrictMode>
+			<WagmiConfig client={client}>
+				<RainbowKitProvider chains={chains}>
+					<BuilderDAO collection={collection} chain={chain}>
+						<App component={component} />
+					</BuilderDAO>
+				</RainbowKitProvider>
+			</WagmiConfig>
+		</React.StrictMode>
+	);
+}
