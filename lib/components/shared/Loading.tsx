20fe80befe011055b9060210d187afe7271c060e
ripe <109935398+ripe0x@users.noreply.github.com>
2023-02-17 17:30:32 -0800

    set width on loading image

--- lib/components/shared/Loading.tsx
+++ lib/components/shared/Loading.tsx
@@ -5,7 +5,14 @@ type Props = {};
 function Loading({}: Props) {
 	return (
 		<div className="mx-auto w-full h-full flex items-center justify-center p-4 md:p-10">
-			<img src={loadingNoggles} alt="loading" className="w-full max-w-[120px]" />
+			<img
+				src={loadingNoggles}
+				alt="loading"
+				className="w-full"
+				style={{
+					maxWidth: '120px',
+				}}
+			/>
 		</div>
 	);
 }
